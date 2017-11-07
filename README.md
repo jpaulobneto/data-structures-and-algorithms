5387ab72b54851c2b66820171fd91425a300858d
Joao Paulo Barbosa <jpaulobneto@gmail.com>
2017-11-07 13:54:22 -0200

    Update README and project index

--- README.md
+++ README.md
@@ -1,13 +1,9 @@
 # data-structures-and-algorithms
-Data structures and algorithms with JavaScript
+Data structures and algorithms with JavaScript.
 
 ## WIP
 
 - [x] Linked Lists
+- [x] Queue
 - [x] Sorted Linked Lists
-- [ ] Doubly Linked Lists
 - [x] Stack
-
-## TODO
-
-- [ ] Extract LinkedList.addSorted() method and create SortedLinkedList class
--- index.js
+++ index.js
@@ -1,51 +1,10 @@
 const { LinkedList, SortedList } = require('./list');
+const Queue = require('./queue');
 const Stack = require('./stack');
 
-console.log('\n===> List');
-
-const linkedList = new LinkedList();
-// add 3 values
-linkedList.add(1);
-linkedList.add(2, 1);
-linkedList.add(3, 2);
-console.log(linkedList.getValues().join(' -> '));
-
-// remove from begin
-linkedList.remove(1);
-console.log(linkedList.getValues().join(' -> '));
-// undo
-linkedList.add(1);
-console.log(linkedList.getValues().join(' -> '));
-
-// remove from middle
-linkedList.remove(2);
-console.log(linkedList.getValues().join(' -> '));
-// undo
-linkedList.add(2, 1);
-console.log(linkedList.getValues().join(' -> '));
-
-// remove from end
-linkedList.remove(3);
-console.log(linkedList.getValues().join(' -> '));
-// undo
-linkedList.add(3, 2);
-console.log(linkedList.getValues().join(' -> '));
-
-console.log('\n===> Sorted List');
-
-const sortedList = new SortedList();
-sortedList.add(2);
-sortedList.add(1);
-sortedList.add(4);
-sortedList.add(3);
-sortedList.add(5);
-console.log(sortedList.getValues().join(' -> '));
-
-console.log('\n===> Stack');
-
-const stack = new Stack();
-stack.push(1);
-stack.push(2);
-stack.push(3);
-console.log(stack.getValues().join(' -> '));
-
+module.exports = {
+  LinkedList,
+  Queue,
+  SortedList,
+  Stack,
+};
