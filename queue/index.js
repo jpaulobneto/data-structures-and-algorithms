d37f081dd9e56a43d219ff609c5674f401dee72d
Joao Paulo Barbosa <jpaulobneto@gmail.com>
2017-11-08 13:05:15 -0200

    Update queue module structure

--- queue/index.js
+++ queue/index.js
@@ -1,48 +1,7 @@
-const Cell = require('../cell/bidirecional-cell');
+const FixedQueue = require('./fixed-queue');
+const Queue = require('./dynamic-queue');
 
-module.exports = class Queue {
-  constructor() {
-    this.head = new Cell();
-    this.tail = new Cell();
-    this.head.next = this.tail;
-    this.tail.prev = this.head;
-    this.size = 0;
-  }
-
-  dequeue() {
-    const { value } = this.head.next;
-
-    this.head.next = this.head.next.next;
-    this.head.next.prev = this.head;
-
-    this.size -= 1;
-
-    return value;
-  }
-
-  enqueue(value) {
-    const newCell = new Cell(value);
-
-    newCell.prev = this.tail.prev;
-    newCell.next = this.tail;
-
-    this.tail.prev.next = newCell;
-    this.tail.prev = newCell;
-
-    this.size += 1;
-  }
-
-  getValues() {
-    const values = [];
-
-    while (!this.isEmpty()) {
-      values.push(this.dequeue());
-    }
-
-    return values;
-  }
-
-  isEmpty() {
-    return this.tail.prev.value == null;
-  }
+module.exports = {
+  FixedQueue,
+  Queue,
 };
